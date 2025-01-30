3cf01c79f01f78944eed7342573039a0b1c55726
JackAtlas <jackatlas8864@gmail.com>
2025-01-30 21:17:20 +0800

    postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,10 @@
 /* eslint-env node */
 module.exports = {
-  plugins: [require('postcss-each')],
+  plugins: [
+    require('postcss-each')({
+      plugins: {
+        beforeEach: [require('postcss-for'), require('postcss-color-mix')],
+      },
+    }),
+  ],
 }
