6643e4c35b45359646622fbae1ecf9a8177654fb
JackAtlas <jackatlas8864@gmail.com>
2025-02-03 19:57:16 +0800

    divider

--- src/components/Divider/Divider.vue
+++ src/components/Divider/Divider.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="className">
-    <div class="chi-divider__text">
+    <div class="chi-divider__text" v-if="!vertical">
       <slot />
     </div>
   </div>
@@ -8,7 +8,7 @@
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import type { DividerProps } from './types'
+import type { ClassType, DividerProps } from './types'
 
 defineOptions({
   name: 'chi-divider',
@@ -18,14 +18,20 @@ const props = defineProps<DividerProps>()
 const slots = defineSlots()
 
 const className = computed(() => {
-  const { textPosition, vertical } = props
-  const className = {
+  const { dashed, textPosition, vertical } = props
+  const className: ClassType = {
     'chi-divider': true,
-    'chi-divider--horizontal': !vertical,
-    'chi-divider--vertical': vertical,
-    'chi-divider--with-text': slots.default,
-    'chi-divider--with-text-left': textPosition === 'left',
-    'chi-divider--with-text-right': textPosition === 'right',
+    'chi-divider--dashed': dashed,
+  }
+  if (vertical) {
+    className['chi-divider--vertical'] = true
+  } else {
+    className['chi-divider--horizontal'] = true
+    if (slots['default']) {
+      className['chi-divider--with-text'] = true
+      if (textPosition === 'left') className['chi-divider--with-text-left'] = true
+      if (textPosition === 'right') className['chi-divider--with-text-right'] = true
+    }
   }
   return className
 })
--- src/components/Divider/types.ts
+++ src/components/Divider/types.ts
@@ -5,3 +5,13 @@ export interface DividerProps {
   textPosition?: 'left' | 'center' | 'right'
   vertical?: boolean
 }
+
+export interface ClassType {
+  'chi-divider': boolean
+  'chi-divider--dashed'?: boolean
+  'chi-divider--horizontal'?: boolean
+  'chi-divider--vertical'?: boolean
+  'chi-divider--with-text'?: boolean
+  'chi-divider--with-text-left'?: boolean
+  'chi-divider--with-text-right'?: boolean
+}
