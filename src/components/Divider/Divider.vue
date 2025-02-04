df885a272f7ff57d12e86688b49c6f8cd257de54
JackAtlas <jackatlas8864@gmail.com>
2025-02-04 20:07:01 +0800

    divider margin

--- src/components/Divider/Divider.vue
+++ src/components/Divider/Divider.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="className">
+  <div role="separator" :class="className" :style="style">
     <div class="chi-divider__text" v-if="!vertical">
       <slot />
     </div>
@@ -16,6 +16,30 @@ defineOptions({
 
 const props = defineProps<DividerProps>()
 const slots = defineSlots()
+
+const style = computed(() => {
+  const style: Record<string, string> = {}
+  if (props.margin) {
+    if (props.vertical) {
+      if (typeof props.margin === 'string') {
+        style['margin-left'] = props.margin
+        style['margin-right'] = props.margin
+      } else if (typeof props.margin === 'number') {
+        style['margin-left'] = props.margin + 'px'
+        style['margin-right'] = props.margin + 'px'
+      }
+    } else {
+      if (typeof props.margin === 'string') {
+        style['margin-top'] = props.margin
+        style['margin-bottom'] = props.margin
+      } else if (typeof props.margin === 'number') {
+        style['margin-top'] = props.margin + 'px'
+        style['margin-bottom'] = props.margin + 'px'
+      }
+    }
+  }
+  return style
+})
 
 const className = computed(() => {
   const { dashed, primary, textPosition, vertical } = props
