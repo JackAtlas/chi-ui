504b43fe2533a5d884718189c992ffeea4bda5b5
JackAtlas <jackatlas8864@gmail.com>
2025-02-15 13:34:26 +0800

    blockquote type

--- src/components/Typography/Blockquote.vue
+++ src/components/Typography/Blockquote.vue
@@ -1,11 +1,25 @@
 <template>
-  <blockquote class="chi-blockquote">
+  <blockquote :class="classname">
     <slot />
   </blockquote>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import type { BlockquoteProps } from './types'
+
 defineOptions({
   name: 'chi-blockquote',
+})
+const props = defineProps<BlockquoteProps>()
+
+const classname = computed(() => {
+  const { type } = props
+  const classname: Record<string, boolean> = {
+    'chi-blockquote': true,
+  }
+  if (type) classname[`chi-blockquote-${type}`] = true
+
+  return classname
 })
 </script>
