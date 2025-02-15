bd2297b68c9ba6fcd1b0f62a86eba8e9d23cd0e0
JackAtlas <jackatlas8864@gmail.com>
2025-02-15 10:34:08 +0800

    docs typography text component

--- docs/zh-CN/components/typography.md
+++ docs/zh-CN/components/typography.md
@@ -19,6 +19,14 @@ Chi UI 内置了一些与标签同名的组件，用以快速应用组件库的�
 
 <preview path="../demo/Typography/Basic-Typography.vue" title="基础用法" description="Chi UI 内置了一些与标签同名的组件，用以快速应用组件库的样式。"></preview>
 
+### 文本组件
+
+提供了不同样式的文本。
+
+当创建段落或加粗文本时可以使用快捷组件 P 和 Strong。
+
+<preview path="../demo/Typography/Typography-Text.vue" title="文本组件" description="提供了不同样式的文本。当创建段落或加粗文本时可以使用快捷组件 P 和 Strong。"></preview>
+
 ### 标题组件
 
 快速创建不同级别的标题，如果你想要更快，可以使用 H1、H2、H3、H4、H5、H6 快捷组件。
---
+++ docs/zh-CN/demo/Typography/Typography-Text.vue
@@ -0,0 +1,42 @@
+<script setup>
+const types = ['default', 'primary', 'info', 'success', 'warning', 'error']
+</script>
+
+<template>
+  <div v-for="textType in types" :key="textType">
+    <chi-text :type="textType">
+      {{ textType.charAt(0).toLocaleUpperCase() + textType.substring(1) }}
+    </chi-text>
+  </div>
+  <div>
+    <chi-text type="disabled">Disabled</chi-text>
+  </div>
+  <div v-for="textType in types" :key="textType">
+    <chi-text mark :type="textType">
+      {{ `Mark ${textType.charAt(0).toLocaleUpperCase()}${textType.substring(1)}` }}
+    </chi-text>
+  </div>
+  <div v-for="textType in types" :key="textType">
+    <chi-text mark reversed :type="textType">
+      {{ `Mark ${textType.charAt(0).toLocaleUpperCase()}${textType.substring(1)}` }}
+    </chi-text>
+  </div>
+  <div>
+    <chi-text code>code</chi-text>
+  </div>
+  <div>
+    <chi-text underline>Underline</chi-text>
+  </div>
+  <div>
+    <chi-text delete>Delete</chi-text>
+  </div>
+  <div>
+    <chi-text strong>Strong</chi-text>
+  </div>
+  <div>
+    <chi-text italic>Italic</chi-text>
+  </div>
+  <div>
+    <chi-text keyboard>Keyboard</chi-text>
+  </div>
+</template>
