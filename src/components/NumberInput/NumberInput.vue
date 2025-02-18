3d0e205c352a23e0b17d0278b7e2ea97b245da55
JackAtlas <jackatlas8864@gmail.com>
2025-02-18 16:10:13 +0800

    update numberinput

--- src/components/NumberInput/NumberInput.vue
+++ src/components/NumberInput/NumberInput.vue
@@ -2,10 +2,13 @@
   <div :class="inputClassname">
     <input
       class="chi-number-input__control"
+      ref="control"
       type="text"
       :disabled="disabled"
       :id="controlId"
       :placeholder="placeholder"
+      @change="handleChange"
+      @input="handleInput"
     />
     <div class="chi-number-input__plus" aria-label="增加" :aria-controls="controlId">
       <Icon name="chevron-up" />
@@ -17,18 +20,35 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-import { getGlobalCount } from '../../utils'
-import type { NumberInputProps } from './types'
+import { computed, onMounted, ref, watch } from 'vue'
+import { boundRange, getGlobalCount, isNull, isValidNumber, toFixed, toNumber } from '../../utils'
+import type { NumberInputEmits, NumberInputProps } from './types'
 import Icon from '../Icon/Icon.vue'
+
+type InputEventType = 'input' | 'change'
+
+const isEmpty = (value: unknown) => !value && value !== 0
+const isNullOrNaN = (value: unknown) => isNull(value) || Number.isNaN(value)
 
 defineOptions({
   name: 'chi-number-input',
 })
 const props = withDefaults(defineProps<NumberInputProps>(), {
+  max: Infinity,
+  min: -Infinity,
   placeholder: '请输入数字',
+  precision: -1,
   value: null,
 })
+const emits = defineEmits<NumberInputEmits>()
+
+const control = ref<HTMLInputElement>()
+const currentValue = ref<string | number>(
+  isEmpty(props.value) ? getEmptyValue() : toNumber(props.value),
+)
+const inputting = ref(false)
+
+let lastValue: number
 
 const inputClassname = computed(() => {
   const { disabled, size } = props
@@ -44,5 +64,154 @@ const inputClassname = computed(() => {
 const idIndex = `${getGlobalCount()}`
 const controlId = `chi-number-input-${idIndex}__control`
 
-console.log(idIndex)
+const preciseNumber = computed(() => {
+  return !inputting.value &&
+    typeof currentValue.value === 'number' &&
+    !Number.isNaN(currentValue.value) &&
+    props.precision >= 0
+    ? toFixed(currentValue.value, props.precision)
+    : currentValue.value
+})
+
+const formattedValue = computed(() => {
+  if (isNullOrNaN(preciseNumber.value) || typeof preciseNumber.value !== 'number') {
+    return preciseNumber.value ?? ''
+  }
+
+  return !inputting.value && typeof props.formatter === 'function'
+    ? props.formatter(preciseNumber.value as number)
+    : preciseNumber.value.toString()
+})
+
+watch(
+  () => props.value,
+  (value) => {
+    if (value !== lastValue) {
+      parseValue()
+    }
+  },
+  { immediate: true },
+)
+
+onMounted(() => {
+  setInputValue(inputting.value ? currentValue.value : formattedValue.value)
+})
+
+defineExpose({
+  input: control,
+  blur: () => control.value?.blur(),
+})
+
+function setInputValue(value?: number | string | null) {
+  if (control.value) {
+    control.value.value = isNullOrNaN(value) ? '' : value!.toString()
+  }
+}
+
+function boundValueRange(value: number) {
+  return boundRange(value, props.min, props.max)
+}
+
+function parseValue() {
+  let value = props.value
+  value = inputting.value
+    ? isEmpty(props.value)
+      ? getEmptyValue()
+      : toNumber(props.value)
+    : isValidNumber(value, true)
+      ? toNumber(value)
+      : getEmptyValue()
+
+  if (props.precision >= 0 && !isNullOrNaN(value)) {
+    value = toFixed(boundValueRange(value), props.precision)
+  }
+
+  currentValue.value = value
+  lastValue = value
+
+  setInputValue(inputting.value ? value : formattedValue.value)
+}
+
+function handleInput(event: Event) {
+  // TODO: debounce and throttle
+  handleChange(event)
+}
+
+function handleChange(event: Event) {
+  const type = event.type as InputEventType
+  const stringValue = (event.target as HTMLInputElement).value
+
+  let value = stringValue.trim()
+
+  // to rollback invalid value to empty in `<input>` when change
+  if (type === 'change' && stringValue && !isValidNumber(stringValue, true)) {
+    const floatValue = parseFloat(stringValue)
+
+    if (Number.isNaN(floatValue)) {
+      value = ''
+    } else {
+      value = floatValue.toString()
+    }
+  }
+
+  inputting.value = type === 'input'
+
+  setValue(value, type)
+}
+
+function setValue(value: string | number, type: InputEventType, sync = props.sync) {
+  if (type !== 'input') {
+    currentValue.value = isEmpty(value) ? getEmptyValue() : toNumber(value)
+  } else {
+    currentValue.value = value
+  }
+
+  setInputValue(currentValue.value)
+  emitChangeEvent(type, sync)
+}
+
+function getEmptyValue() {
+  switch (props.emptyType) {
+    case 'undefined':
+      return undefined! as number
+    case 'null':
+      return null! as number
+    default:
+      return NaN
+  }
+}
+
+function emitChangeEvent(type: InputEventType, sync = props.sync) {
+  type = type === 'input' ? 'input' : 'change'
+
+  if (type === 'change') {
+    const empty = isEmpty(currentValue.value)
+    const value = empty ? getEmptyValue() : toNumber(currentValue.value)
+
+    let boundValue = empty ? value : boundValueRange(toNumber(value))
+
+    if (!empty && props.precision >= 0) {
+      boundValue = toFixed(boundValue, props.precision)
+    }
+
+    // const changed = !Object.is(boundValue, value)
+
+    if (!empty) {
+      currentValue.value = boundValue
+    }
+
+    if (!sync && Object.is(lastValue, boundValue)) {
+      if (!Object.is(props.value, value)) emits('update:value', boundValue)
+      return
+    }
+
+    lastValue = boundValue
+
+    if (!sync) {
+      emits('update:value', boundValue)
+    }
+  } else {
+    // console.log('input')
+  }
+}
 </script>
--- src/components/NumberInput/types.ts
+++ src/components/NumberInput/types.ts
@@ -3,8 +3,14 @@ export type NumberInputEmptyType = 'NaN' | 'undefined' | 'null'
 
 export interface NumberInputProps {
   disabled?: boolean
+  emptyType?: NumberInputEmptyType
+  formatter?: (number: number) => number
+  max?: number
+  min?: number
   placeholder?: string
+  precision?: number
   size?: 'small' | 'medium' | 'large'
+  sync?: boolean
   value?: number | null
 }
 
