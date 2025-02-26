42ed2ed35ee69812ad938a62fbd1c9d0f3827cb3
JackAtlas <jackatlas8864@gmail.com>
2025-02-26 17:23:54 +0800

    vite package config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    lib: {
+      entry: './src/components/index.ts',
+      name: '@chi/Chi-ui',
+    },
+  },
 })
