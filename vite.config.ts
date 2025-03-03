22b26aaad4a418b42ca1892b3ae69b90aa9e66cb
JackAtlas <jackatlas8864@gmail.com>
2025-03-03 15:09:48 +0800

    try to provide types

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({ tsconfigPath: './tsconfig.app.json' })],
   build: {
+    lib: {
+      entry: resolve(__dirname, 'index.ts'),
+      name: '@jackatlas/chi-ui',
+      fileName: 'chi-ui',
+    },
     rollupOptions: {
-      external: ['vue'],
+      external: ['vue', 'lucide-vue-next', '@floating-ui/vue'],
       output: {
         globals: {
           vue: 'Vue',
         },
       },
-    },
-    lib: {
-      entry: './src/components/index.ts',
-      name: '@jackatlas/chi-ui',
     },
   },
 })
