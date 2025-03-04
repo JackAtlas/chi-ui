3e7cff5c7fc444c33f474bad7799f417164251b6
JackAtlas <jackatlas8864@gmail.com>
2025-03-04 11:02:45 +0800

    1.2.3 add types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+declare module 'vue' {
+  export interface GlobalComponents {
+    'chi-button': (typeof import('@jackatlas/chi-ui'))['chi-button']
+    'chi-cell': (typeof import('@jackatlas/chi-ui'))['chi-cell']
+    'chi-divider': (typeof import('@jackatlas/chi-ui'))['chi-divider']
+    'chi-grid': (typeof import('@jackatlas/chi-ui'))['chi-grid']
+    'chi-icon': (typeof import('@jackatlas/chi-ui'))['chi-icon']
+    'chi-linker': (typeof import('@jackatlas/chi-ui'))['chi-linker']
+    'chi-masker': (typeof import('@jackatlas/chi-ui'))['chi-masker']
+    'chi-select': (typeof import('@jackatlas/chi-ui'))['chi-select']
+    'chi-space': (typeof import('@jackatlas/chi-ui'))['chi-space']
+    'chi-title': (typeof import('@jackatlas/chi-ui'))['chi-title']
+    'chi-text': (typeof import('@jackatlas/chi-ui'))['chi-text']
+    'chi-blockquote': (typeof import('@jackatlas/chi-ui'))['chi-blockquote']
+    'chi-ol': (typeof import('@jackatlas/chi-ui'))['chi-ol']
+    'chi-ul': (typeof import('@jackatlas/chi-ui'))['chi-ul']
+    'chi-h1': (typeof import('@jackatlas/chi-ui'))['chi-h1']
+    'chi-h2': (typeof import('@jackatlas/chi-ui'))['chi-h2']
+    'chi-h3': (typeof import('@jackatlas/chi-ui'))['chi-h3']
+    'chi-h4': (typeof import('@jackatlas/chi-ui'))['chi-h4']
+    'chi-h5': (typeof import('@jackatlas/chi-ui'))['chi-h5']
+    'chi-h6': (typeof import('@jackatlas/chi-ui'))['chi-h6']
+    'chi-p': (typeof import('@jackatlas/chi-ui'))['chi-p']
+    'chi-strong': (typeof import('@jackatlas/chi-ui'))['chi-strong']
+  }
+}
+
+export {}
